ac481c1806dfdd19aed3e91d07dfb9f6169a6eeb
Senorgeno <genedower@gmail.com>
2013-07-02 20:01:09 +1200

    initial commit

---
+++ csvue/_config.php
@@ -0,0 +1,22 @@
+<?php
+
+global $project;
+$project = 'csvue';
+
+global $database;
+$database = 'webstudios_csvue';
+
+require_once('conf/ConfigureFromEnv.php');
+
+
+MySQLDatabase::set_connection_charset('utf8');
+
+// Set the current theme. More themes can be downloaded from
+// http://www.silverstripe.org/themes/
+SSViewer::set_theme('simple');
+
+// Set the site locale
+i18n::set_locale('en_GB');
+
+// Enable nested URLs for this site (e.g. page/sub-page/)
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
