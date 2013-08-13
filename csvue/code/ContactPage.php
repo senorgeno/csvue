a12241ea3450eb15241765eccd106d8b963b9cab
Cameron Grant <sasky.creative@gmail.com>
2013-08-13 19:49:22 +1200

    contact Page

--- csvue/code/ContactPage.php
+++ csvue/code/ContactPage.php
@@ -12,28 +12,44 @@ class ContactPage_Controller extends Page_Controller {
 
 	public function ContactForm() {
 		$fields =  new FieldList(array(
-			TextField::create('Name','Your Name' )->setAttribute('required', true)->addExtraClass('fieldadjust'),
-			EmailField::create('Email')->setAttribute('required', true)->addExtraClass('fieldadjust'),
-			TextField::create('Phone', 'Phone (optional)')->addExtraClass('fieldadjust'),
-			TextareaField::create('Message')->setAttribute('required', true)->addExtraClass('fieldadjust')
+			TextField::create('CompanyName','Company Name *' )->setAttribute('required', true)->addExtraClass('fieldadjust'),
+			TextField::create('firstName','First Name *' )->setAttribute('required', true)->addExtraClass('fieldadjust'),
+			TextField::create('lastName','Last Name *' )->setAttribute('required', true)->addExtraClass('fieldadjust'),
+			TextField::create('jobTitle','Job Title *' )->setAttribute('required', true)->addExtraClass('fieldadjust'),
+			EmailField::create('Email','E-mail address *')->setAttribute('required', true)->addExtraClass('fieldadjust'),
+			TextField::create('Phone','Contact Number *')->setAttribute('required', true)->addExtraClass('fieldadjust'),
+			TextField::create('postal1','Postal Address 1')->addExtraClass('fieldadjust'),
+			TextField::create('postal2','Postal Address 2')->addExtraClass('fieldadjust'),
+			CountryDropdownField::create('Country','Country',null,'NZ')->addExtraClass('fieldadjust'),
+			TextareaField::create('Message','Enter your Message *')->setAttribute('required', true)->addExtraClass('fieldadjust'),
+			CheckboxField::create('futureInfo','Tick to receive future info')->addExtraClass('fieldadjust')
 		));
 		$actions = new FieldList(array(
 			FormAction::create('doContactForm', 'Contact CS-VUE')->addExtraClass('button rounded')
 		));
-		$validator = new RequiredFields('Name','Email','Message');
+		$validator = new RequiredFields('CompanyName','firstName','lastName','jobTitle','Email','Phone','Message');
 		return new Form($this,'ContactForm',$fields, $actions, $validator);
 	}
 
 	public function doContactForm($data, $form) {
 		$to = SiteConfig::get()->first()->email;
-		$body = '<h1> ' . $data['Name'] . '</h1>
-				 <p> ' . $data['Email'] . '</p>
-				 <p> ' . $data['Message'] . '</p>';
-		if($data['Phone']) {
-			$body .= '<p>' .$data['Phone']  . '</p>';
-		}
+		$country = Zend_Locale::getTranslation($data['Country'], 'Country');
+		$futureinfo = $data['futureInfo'] ? 'has signed up for Future Info ' : 'Has not signed up for future info';
 
-		$email = new Email($data['Email'],$to, 'enquiry form the CSVUE Website', $body );
+
+		$body = '<h1> ' . $data['CompanyName'] . '</h1>
+				 <p> ' . $data['firstName'] . '</p>
+				 <p> ' . $data['lastName'] . '</p>
+				 <p> ' . $data['jobTitle'] . '</p>
+				 <p> ' . $data['Email'] . '</p>
+				 <p> ' . $data['Phone'] . '</p>
+ 				 <p> ' . $data['postal1'] . '</p>
+ 				 <p> ' . $data['postal2'] . '</p>
+ 				 <p> ' . $country . 		'</p>
+				 <p> ' . $futureinfo . '</p>';
+
+
+		$email = new Email($to,$to, 'enquiry form the CSVUE Website', $body );
 		$email->send();
 		$form->sessionMessage('Thanks, we\'ll be in touch', 'good');
 		return $this->redirectBack();
